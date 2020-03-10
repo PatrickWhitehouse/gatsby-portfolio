84c30ca7f6b5fd1aa8e0fd84788e672202e39619
Patrick Whitehouse <patrick.whitehouse@pushon.co.uk>
2020-03-10 12:30:58 +0000

    begin blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from "../components/layout";
+
+const Blog = () => (
+    <Layout>
+        <h1 className="text-4xl mb-3">Blog Posts</h1>
+        <p>Have a read of my latest blog posts about web development, cars and the odd ramble about general life stuff.</p>
+    </Layout>
+)
+
+export default Blog;
