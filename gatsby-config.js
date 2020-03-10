90eb878734e9a29a259bc80cf8d12d0a103a9714
Patrick Whitehouse <patrick.whitehouse@pushon.co.uk>
2020-03-10 13:26:37 +0000

    get dynamic blogs working

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,14 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import Layout from "../components/layout";
+import { graphql, Link } from 'gatsby';
 
-const Blog = () => (
+const Blog = ({ data: { allMdx } }) => (
     <Layout>
         <h1 className="text-4xl mb-3">Blog Posts</h1>
         <p>Have a read of my latest blog posts about web development, cars and the odd ramble about general life stuff.</p>
+        {allMdx.edges.map(({ node }) => (
+            <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+        ))}
     </Layout>
 )
 
 export default Blog;
+
+export const blogCall = graphql`
+query blogCall {
+  allMdx {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          path
+          date
+        }
+      }
+    }
+  }
+}
+
+`
