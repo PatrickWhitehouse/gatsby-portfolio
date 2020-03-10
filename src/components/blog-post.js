f33a42c51c8b5538aa7c07f2fb08fbaede92f5ba
Patrick Whitehouse <patrick.whitehouse@pushon.co.uk>
2020-03-10 13:36:11 +0000

    move graphql

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -3,12 +3,12 @@ import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from './layout'
 export default function PageTemplate({ data: { mdx } }) {
-    return (
-        <Layout>
-            <h1>{mdx.frontmatter.title}</h1>
-            <MDXRenderer>{mdx.body}</MDXRenderer>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <h1>{mdx.frontmatter.title}</h1>
+      <MDXRenderer>{mdx.body}</MDXRenderer>
+    </Layout>
+  )
 }
 
 export const pageQuery = graphql`
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import Layout from "../components/layout";
-import { graphql, Link } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 
-const Blog = ({ data: { allMdx } }) => (
-    <Layout>
-        <h1 className="text-4xl mb-3">Blog Posts</h1>
-        <p>Have a read of my latest blog posts about web development, cars and the odd ramble about general life stuff.</p>
-        {allMdx.edges.map(({ node }) => (
-            <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
-        ))}
-    </Layout>
-)
-
-export default Blog;
-
-export const blogCall = graphql`
+const Blog = () => {
+    const blogCall = useStaticQuery(graphql`
 query blogCall {
   allMdx {
     edges {
@@ -29,5 +18,18 @@ query blogCall {
     }
   }
 }
-
 `
+    );
+    const { allMdx: { edges } } = blogCall;
+    return (
+        <Layout>
+            <h1 className="text-4xl mb-3">Blog Posts</h1>
+            <p>Have a read of my latest blog posts about web development, cars and the odd ramble about general life stuff.</p>
+            {edges.map(({ node }) => (
+                node.id
+            ))}
+        </Layout>
+    )
+}
+
+export default Blog;
