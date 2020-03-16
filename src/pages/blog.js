67909bde2e380a4de7016b66666753a7979e3e6e
Patrick Whitehouse <patrick1996@gmail.com>
2020-03-16 20:44:48 +0000

    make colour pull from frontmatter

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,45 +1,52 @@
-import React from 'react';
-import Layout from "../components/layout";
-import { useStaticQuery, graphql, Link } from 'gatsby';
+import React from "react"
+import Layout from "../components/layout"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 const Blog = () => {
-    const blogCall = useStaticQuery(graphql`
-query blogCall {
-  allMdx {
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-          path
-          date
-          description
+  const blogCall = useStaticQuery(graphql`
+    query blogCall {
+      allMdx {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              path
+              date
+              description
+              css
+            }
+          }
         }
       }
     }
-  }
+  `)
+  const {
+    allMdx: { edges },
+  } = blogCall
+  return (
+    <Layout>
+      <h1 className="text-4xl mb-3">Blog Posts</h1>
+      <p>
+        Have a read of my latest blog posts about web development, cars and the
+        odd ramble about general life stuff.
+      </p>
+      {edges.map(({ node }, i) => (
+        <Link key={i} to={node.frontmatter.path}>
+          <div
+            className={`w-1/2 my-5 text-${node.frontmatter.css}-400 border border-${node.frontmatter.css}-400 rounded p-4 flex flex-col justify-between leading-normal`}
+          >
+            <div className="mb-8">
+              <div className="font-bold text-xl mb-2">
+                {node.frontmatter.title}
+              </div>
+              <p className="text-base">{node.frontmatter.description}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </Layout>
+  )
 }
-`
-    );
-    const { allMdx: { edges } } = blogCall;
-    return (
-        <Layout>
-            <h1 className="text-4xl mb-3">Blog Posts</h1>
-            <p>Have a read of my latest blog posts about web development, cars and the odd ramble about general life stuff.</p>
-            {edges.map(({ node }) => (
 
-                <Link key={node.id} to={node.frontmatter.path}>
-                    <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                        <div className="mb-8">
-                            <div className="text-gray-900 font-bold text-xl mb-2">{node.frontmatter.title}</div>
-                            <p className="text-gray-700 text-base">{node.frontmatter.description}</p>
-                        </div>
-                    </div>
-                </Link>
-
-            ))}
-        </Layout>
-    )
-}
-
-export default Blog;
+export default Blog
